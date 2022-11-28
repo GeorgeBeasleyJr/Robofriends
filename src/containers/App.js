2002b58fe39d7c11ac5897133872d38c8099c2fb
George W Beasley Jr <117643975+GeorgeBeasleyJr@users.noreply.github.com>
2022-11-27 19:45:31 -0700

    Creating a new repository and adding all the files for Robofriends.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -30,17 +31,19 @@ class App extends Component {
                 .toLocaleLowerCase()
                 .includes(searchfield.toLocaleLowerCase());
         })
-        return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
+        return !robots.length ? (
+            <h1>Loading</h1>
+        ) : (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
                         <CardList robots={filteredRobots} />
-                    </Scroll>
-                </div>
-            );
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
     }
 }
 
